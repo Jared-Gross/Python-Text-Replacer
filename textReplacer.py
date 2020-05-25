9ab888d5303df12fb3122e4775735932e37d26c3
JareBear <jaredgrozz@gmail.com>
2020-05-25 13:19:17 -0500

    Update textReplacer.py

--- textReplacer.py
+++ textReplacer.py
@@ -2,15 +2,15 @@
 wordsToReplaceOld = []
 wordsToReplaceNew = []
 content = []
+file_to_edit = 'textTest.txt'
 with open('textToReplace.txt') as f:
-    content = f.readlines()
-    for i in content:
+    for i in  f.readlines():
         if i != '\n':
             wordList = i.split('=')
             wordsToReplaceOld.append(wordList[0])
             wordsToReplaceNew.append(wordList[1].replace('\n',''))
-with open('textTest.txt', 'r') as f: content = f.readlines()
-with open('textTest.txt', 'w+') as f:
+with open(file_to_edit, 'r') as f: content = f.readlines()
+with open(file_to_edit, 'w+') as f:
     for line, word in enumerate(content):
         for index, wordToReplace in enumerate(wordsToReplaceOld):
             if word.__contains__(wordToReplace):   word = word.replace(wordToReplace,wordsToReplaceNew[index]); continue
